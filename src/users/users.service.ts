15f6acd6e84284cb0bdb2e71dba82ca46dafc2a8
MembaMcetywa <membatheone@gmail.com>
2025-06-30 12:08:30 +0200

    clean up

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,4 @@
-import {
-  ConflictException,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
